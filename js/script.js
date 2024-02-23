023f74e36163cdd2d59f5e31142347aec42c00da
Foysal Hasib <118272136+foysalhasib@users.noreply.github.com>
2024-02-23 19:28:47 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,21 @@
+//================== Object_ClassName ========================
+
+class Person {
+    constructor(fname,lname,home){
+        this.FirstName=fname;
+        this.LastName=lname;
+        this.home=home;
+    }
+}
+
+let person1 = new Person ("Foysal","Hasib","Mirpur")
+let person2 = new Person ("Saleh","Akram","Agargaon")
+let person3 = new Person ("MD.","Masum","Gazipur")
+let person4 = new Person ("Rokib","Hasan","Komlapur")
+
+
+
+console.log(person1);
+console.log(person2);
+console.log(person3);
+console.log(person4);
